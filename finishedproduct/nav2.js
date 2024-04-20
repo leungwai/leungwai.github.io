267a8fc3d3aa6671e51d1d6674d964a8ce55b50b
Leung Wai Liu <liuleungwai@gmail.com>
2024-04-20 11:06:35 -0400

    Fixed WebKit Compatibility

--- finishedproduct/nav2.js
+++ finishedproduct/nav2.js
@@ -1,5 +1,6 @@
 const primaryNav = document.querySelector('.mobile-header-bar');
 const navToggle = document.querySelector('.menu-button');
+const menuButton = document.getElementById('mobile-menu-button');
 const bottomNav = document.querySelector('.bottom-bar');
 
 // data visble attribute hooks into HMTL for JS
@@ -11,12 +12,14 @@ navToggle.addEventListener('click', () => {
         bottomNav.setAttribute("data-visible", false);
         
         navToggle.textContent = "close";
+        menuButton.style.textDecoration = "underline";
         navToggle.setAttribute("aria-expanded", true);
     } else if (visibility === "true") {
         primaryNav.setAttribute("data-visible", false);
         bottomNav.setAttribute("data-visible", true);
 
         navToggle.textContent = "menu";
+        menuButton.style.textDecoration = "none";
         navToggle.setAttribute("aria-expanded", false);
     }
 });
